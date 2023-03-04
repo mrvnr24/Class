0775af6557405b98c797a2523f48b8fc383e9e3e
Mervenur Aytekin <mrvnre24@gmail.com>
2023-03-03 21:31:42 -0600

    class examples

---
+++ force-app/main/default/lwc/lwc12/lwc12.js
@@ -0,0 +1,8 @@
+import { LightningElement, wire } from 'lwc';
+import getAccountList from '@salesforce/apex/GetAccount.getAccountList';
+export default class Lwc12 extends LightningElement {
+
+@wire(getAccountList) Accounts;
+
+
+}
